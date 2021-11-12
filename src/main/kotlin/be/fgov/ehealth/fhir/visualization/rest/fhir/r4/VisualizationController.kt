507cc9efc583fc6dd2137f7eccbc14abbfcce553
Antoine Duchâteau <ad@taktik.com>
2021-11-12 14:46:26 +0100

    Add endpoints for multipart

--- src/main/kotlin/be/fgov/ehealth/fhir/visualization/rest/fhir/r4/VisualizationController.kt
+++ src/main/kotlin/be/fgov/ehealth/fhir/visualization/rest/fhir/r4/VisualizationController.kt
@@ -6,12 +6,11 @@ import ca.uhn.fhir.context.FhirContext
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import mergeDataBuffers
 import org.hl7.fhir.r4.model.Bundle
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.http.server.reactive.ServerHttpResponse
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 
 @ExperimentalStdlibApi
@@ -22,24 +21,38 @@ import reactor.core.publisher.Mono
 class VisualizationController(val diagnosticReportHtmlGenerator: DiagnosticReportHtmlGenerator) {
     @PostMapping("/html", consumes = ["application/json", "application/xml"])
     @Operation(summary = "Convert FHIR file to html")
-    fun html(@RequestBody fhirFile: ByteArray, response: ServerHttpResponse): Mono<Void> {
-        val ctx = FhirContext.forR4()
-        val parser = ctx.newJsonParser()
-        val bundle = stripNarratives(ctx, parser.parseResource(Bundle::class.java, fhirFile.toString(Charsets.UTF_8)))
-        val html = diagnosticReportHtmlGenerator.generateHtmlRepresentation(ctx, bundle, null)
-        response.headers.set("Content-Type", "text/html")
-        return response.writeWith(Mono.just(response.bufferFactory().wrap(html)))
-    }
+    fun html(@RequestBody fhirFile: ByteArray, response: ServerHttpResponse): Mono<Void> =
+        makeResponse(fhirFile, response, true)
 
     @PostMapping("/div", consumes = ["application/json", "application/xml"])
     @Operation(summary = "Convert FHIR file to embeddable div")
-    fun div(@RequestBody fhirFile: ByteArray, response: ServerHttpResponse): Mono<Void> {
+    fun div(@RequestBody fhirFile: ByteArray, response: ServerHttpResponse): Mono<Void> =
+        makeResponse(fhirFile, response, false)
+
+    @PostMapping("/html/single")
+    @Operation(summary = "Convert FHIR file to html")
+    fun htmlMultipartSingle(@RequestPart("file") fhirFilePart: Mono<FilePart>, response: ServerHttpResponse): Mono<Void> =
+        fhirFilePart.flatMap { it.content().mergeDataBuffers() }.flatMap { fhirFile -> makeResponse(fhirFile, response, true) }
+
+    @PostMapping("/div/single")
+    @Operation(summary = "Convert FHIR file provided as multipart request to embeddable div")
+    fun divMultipartSingle(@RequestPart("file") fhirFilePart: Mono<FilePart>, response: ServerHttpResponse) =
+        fhirFilePart.flatMap { it.content().mergeDataBuffers() }.flatMap { fhirFile -> makeResponse(fhirFile, response, false) }
+
+    private fun makeResponse(
+        fhirData: ByteArray,
+        response: ServerHttpResponse,
+        embedInHtml: Boolean
+    ): Mono<Void> {
         val ctx = FhirContext.forR4()
         val parser = ctx.newJsonParser()
-        val bundle = stripNarratives(ctx, parser.parseResource(Bundle::class.java, fhirFile.toString(Charsets.UTF_8)))
-        val div = diagnosticReportHtmlGenerator.generateDivRepresentation(ctx, bundle, null).toByteArray(Charsets.UTF_8)
+        val bundle = stripNarratives(ctx, parser.parseResource(Bundle::class.java, fhirData.toString(Charsets.UTF_8)))
+        val htmlOrDiv = if (embedInHtml) {
+            diagnosticReportHtmlGenerator.generateHtmlRepresentation(ctx, bundle, null)
+        } else {
+            diagnosticReportHtmlGenerator.generateDivRepresentation(ctx, bundle, null).toByteArray(Charsets.UTF_8)
+        }
         response.headers.set("Content-Type", "text/html")
-        return response.writeWith(Mono.just(response.bufferFactory().wrap(div)))
+        return response.writeWith(Mono.just(response.bufferFactory().wrap(htmlOrDiv)))
     }
-
 }
