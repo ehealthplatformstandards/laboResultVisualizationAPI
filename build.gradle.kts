febc32f8f0d414834628fec9212a9530dfe6c96c
Etienne Cantineau <etienne.cantineau@ehealth.fgov.be>
2023-07-13 11:17:20 +0200

    upgraded dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.spring") version "1.5.31"
-    id("org.springframework.boot") version "2.6.2"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.springframework.boot") version "2.7.11"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     id("cz.augi.docker-java") version "3.1.0"
      id("com.palantir.git-version") version "0.15.0"
 }
@@ -15,7 +15,8 @@ buildscript {
 
     repositories {
         mavenCentral()
-        maven { url = uri("https://ehealthplatformstandards.github.io/laboResultVisualization/") }
+		maven { url = uri("file://C:/Users/eh068/Documents/LaboResultVisualizer/laboResultVisualization2/target/mvn-repo") }
+        //maven { url = uri("https://ehealthplatformstandards.github.io/laboResultVisualization/") }
         //maven { url = uri("file://C:/Temp/toy/laboResultVisualization/target/mvn-repo") }
     }
     /*
@@ -31,7 +32,7 @@ buildscript {
 val repoPassword: String by project
 val mavenReleasesRepository: String by project
 
-val kotlinCoroutinesVersion = "1.5.2"
+val kotlinCoroutinesVersion = "1.6.4"
 
 //apply(plugin = "git-version")
 
@@ -69,8 +70,9 @@ configure<com.taktik.gradle.plugins.flowr.DockerJavaPluginExtension> {
 */
 repositories {
     mavenCentral()
+	maven { url = uri("file://C:/Users/eh068/Documents/LaboResultVisualizer/laboResultVisualization2/target/mvn-repo") }
     //maven { url = uri("file://C:/Temp/toy/laboResultVisualization/target/mvn-repo") }
-    maven { url = uri("https://ehealthplatformstandards.github.io/laboResultVisualization/") }
+    //maven { url = uri("https://ehealthplatformstandards.github.io/laboResultVisualization") }
     /*
     maven {
         credentials {
@@ -85,8 +87,8 @@ repositories {
 dependencies {
     implementation("be.fgov.ehealth:fhir-visualization-tool:1.13")
     //implementation("io.icure:async-jackson-http-client:0.1.4-6cab16ec6e")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:4.1.0")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-base:4.1.0")
+    implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:6.6.1")
+    implementation("ca.uhn.hapi.fhir:hapi-fhir-base:6.6.1")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -98,21 +100,21 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.12.5")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.0")
 
-    implementation(group = "com.github.ben-manes.caffeine", name = "caffeine", version = "3.0.4")
+    implementation(group = "com.github.ben-manes.caffeine", name = "caffeine", version = "3.1.5")
 
     // Logging
     implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.10")
     implementation(group = "ch.qos.logback", name = "logback-access", version = "1.2.10")
-    
+
     //implementation(group = "com.taktik.boot", name = "spring-boot-starter-gke-logging", version = "2.1.174-0f038f8004")
 
     // Swagger
-    implementation(group = "org.springdoc", name = "springdoc-openapi-webflux-ui", version = "1.5.11")
-    implementation(group = "org.springdoc", name = "springdoc-openapi-kotlin", version = "1.5.11")
+    implementation(group = "org.springdoc", name = "springdoc-openapi-webflux-ui", version = "1.7.0")
+    implementation(group = "org.springdoc", name = "springdoc-openapi-kotlin", version = "1.7.0")
 
-    implementation(group = "com.github.ben-manes.caffeine", name = "caffeine", version = "3.0.3")
+    implementation(group = "com.github.ben-manes.caffeine", name = "caffeine", version = "3.1.5")
 }
 
 tasks.withType<KotlinCompile> {
