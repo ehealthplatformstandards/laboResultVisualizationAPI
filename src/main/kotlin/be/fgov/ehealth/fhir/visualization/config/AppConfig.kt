14db689b852986896e76a46e57954093e8dc3a9c
Etienne Cantineau <etienne.cantineau@ehealth.fgov.be>
2023-07-13 11:19:39 +0200

    changed DiagnosticReport to generic resource

--- src/main/kotlin/be/fgov/ehealth/fhir/visualization/config/AppConfig.kt
+++ src/main/kotlin/be/fgov/ehealth/fhir/visualization/config/AppConfig.kt
@@ -1,11 +1,11 @@
 package be.fgov.ehealth.fhir.visualization.config
 
-import be.fgov.ehealth.fhir.narrative.gen.DiagnosticReportHtmlGenerator
+import be.fgov.ehealth.fhir.narrative.gen.ResourceHtmlGenerator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class AppConfig {
     @Bean
-    fun diagnosticReportHtmlGenerator() = DiagnosticReportHtmlGenerator()
+    fun resourceHtmlGenerator() = ResourceHtmlGenerator() 
 }
